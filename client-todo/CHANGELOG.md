67d63815afd5499fefbf236ace6e032f6f35b09e
Dina Berry <dinaberry@outlook.com>
2023-12-28 20:02:43 +0000

    client-todo 1.0.1 version - 006 client-todo fix

--- client-todo/CHANGELOG.md
+++ client-todo/CHANGELOG.md
@@ -1,5 +1,12 @@
 ## Todo Client
 
+## 1.0.1
+
+* Remove unnecessary css
+* Set local default value for API URL BASE 
+* ViTest test for local and production
+* CICD has client-todo version as step
+
 ## 1.0.0 
 
 * Initial version of Frontend client
--- client-todo/src/todo/components/item.tsx
+++ client-todo/src/todo/components/item.tsx
@@ -10,11 +10,11 @@ export interface ItemProps {
 export default function Item({ todo, onDelete }: ItemProps) {
 
   return (
-    <tr className="border-b border-gray-300" data-testid="item-row">
-      <td className="px-2 font-bold" data-testid="item-id">{todo.id}</td>
-      <td className="px-2" data-testid="item-title">{todo.title}</td>
-      <td className="px-2" data-testid="item-delete">
-        <button onClick={() => onDelete(todo.id)} className="px-2 py-1 bg-blue-500 text-white border-none rounded cursor-pointer text-xs">X</button>
+    <tr data-testid="item-row">
+      <td data-testid="item-id">{todo.id}</td>
+      <td data-testid="item-title">{todo.title}</td>
+      <td data-testid="item-delete">
+        <button onClick={() => onDelete(todo.id)} >X</button>
       </td>
     </tr>
   );
