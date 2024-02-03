803ea9a47154d8ba06488bb289c1d868da37d0cd
Dina Berry <dinaberry@outlook.com>
2024-02-03 18:39:54 +0000

    008 welcome?

--- .github/workflows/.devcontainer/setup.sh
+++ .github/workflows/.devcontainer/setup.sh
@@ -3,8 +3,5 @@ set -eux
 
 echo "Set up the devcontainer"
 
-# Temp fix for Vite security
-chmod -R +x ./scripts
-
 # Install Azure CLI - feature isn't working correctly
 curl -fsSL https://aka.ms/install-azd.sh | bash
